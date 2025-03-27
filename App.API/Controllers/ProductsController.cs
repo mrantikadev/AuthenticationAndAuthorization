966b7585b781f23330912f412f668a59731032ca
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-03-27 10:41:04 +0300

    Made some methods only accessible to admins in ProductsController

--- App.API/Controllers/ProductsController.cs
+++ App.API/Controllers/ProductsController.cs
@@ -1,7 +1,9 @@
-﻿using App.Services.Products;
+﻿using App.Repositories.Users;
+using App.Services.Products;
 using App.Services.Products.Create;
 using App.Services.Products.Update;
 using App.Services.Products.UpdateStock;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.API.Controllers
@@ -21,15 +23,19 @@ namespace App.API.Controllers
         public async Task<IActionResult> GetById(int id) => CreateActionResult(await productService.GetByIdAsync(id));
 
         [HttpPost]
+        [Authorize(Roles = RoleNames.Admin)]
         public async Task<IActionResult> Create(CreateProductRequest request) => CreateActionResult(await productService.CreateAsync(request));
 
         [HttpPut("{id:int}")]
+        [Authorize(Roles = RoleNames.Admin)]
         public async Task<IActionResult> Update(int id, UpdateProductRequest request) => CreateActionResult(await productService.UpdateAsync(id, request));
 
         [HttpPatch("stock")]
+        [Authorize(Roles = RoleNames.Admin)]
         public async Task<IActionResult> UpdateStock(UpdateProductStockRequest request) => CreateActionResult(await productService.UpdateStockAsync(request));
 
         [HttpDelete("{id:int}")]
+        [Authorize(Roles = RoleNames.Admin)]
         public async Task<IActionResult> Delete(int id) => CreateActionResult(await productService.DeleteAsync(id));
     }
 }
